8cd37e386b916e72efff8aded3d00a3c2833bd5e
Enernity <mdk895795914@163.com>
2019-04-22 13:38:33 +0800

    新建Main方法0422

---
+++ src/mdk/test0422.java
@@ -0,0 +1,13 @@
+package mdk;
+
+/**
+ * @Description ToDo
+ * @Author MDK
+ * @Date 2019/4/22 13:36
+ * @Version 1.0
+ **/
+public class test0422 {
+    public static void main(String[] args) {
+        System.out.println("提示操作");
+    }
+}
