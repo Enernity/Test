a55a6363b67a3571e44775208bdb9a84f6b3b401
Enernity <mdk895795914@163.com>
2019-04-22 16:42:02 +0800

    新建Main方法0422_2

---
+++ src/mdk/test0422_2.java
@@ -0,0 +1,13 @@
+package mdk;
+
+/**
+ * @Description ToDo
+ * @Author MDK
+ * @Date 2019/4/22 16:41
+ * @Version 1.0
+ **/
+public class test0422_2 {
+    public static void main(String[] args) {
+        System.out.println("第二个");
+    }
+}
